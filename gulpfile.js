c777bc72dedf107fa267b7a2d032a4f3c6d0d9ff
Natallia <nzhyrkova@biarum.com>
2016-06-06 17:44:51 +0300

    added html pages without header&footer, added main logo, added gulp-headerfooter

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
+var headerfooter = require('gulp-headerfooter');
+
 
 gulp.task('html', function () {
     return gulp.src('./src/*.html').pipe(gulp.dest('./build'));
 });
 
+gulp.task('headerfooter', function () {
+    gulp.src('./src/*.html')
+        .pipe(headerfooter.header('./src/part/header.html'))
+        .pipe(headerfooter.footer('./src/part/footer.html'))
+        .pipe(gulp.dest('./build/'));
+});
---
+++ src/scripts/scripts.js
@@ -0,0 +1,3 @@
+/**
+ * Created by nzhyrkova on 06.06.2016.
+ */
