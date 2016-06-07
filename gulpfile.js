be3ff7932620d2312be2abc8de2a1143b8c04aaf
Natallia <nzhyrkova@biarum.com>
2016-06-07 16:47:43 +0300

    added gulp.task('less') added semantic styles structure corrected some header's urls

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 var headerfooter = require('gulp-headerfooter');
+var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('html', function () {
@@ -11,4 +14,20 @@ gulp.task('headerfooter', function () {
         .pipe(headerfooter.header('./src/part/header.html'))
         .pipe(headerfooter.footer('./src/part/footer.html'))
         .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('less', function () {
+    return gulp.src('./src/styles/style.less')
+        .pipe(less({
+            paths: [ path.join('./src/styles', 'less', 'includes') ]
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/styles/style.less',['less']);
 });
