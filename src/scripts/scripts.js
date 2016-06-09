9630285d73be90503b09413c0a22a1e664c1cad0
n.zhirkova <neyia@mail.ru>
2016-06-10 00:41:39 +0300

    fancy box

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -163,6 +163,15 @@ $(document).ready(function(){
         //alert('coming soon');
         $('.main-nav--sliding').slideToggle('slow');
     });
-    
-    
+
+    $('.about-room__slider .zoom').fancybox({
+        'transitionIn'  : 'none',
+        'transitionOut' : 'none',
+        'titlePosition' : 'over',
+        'titleFormat'   : function(title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+
+    });
+
 });
