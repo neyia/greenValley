d1973a0382ce61b111d78a7f08f4c3faabd202c9
n.zhirkova <neyia@mail.ru>
2016-06-10 08:53:04 +0300

    restructured js-files

---
+++ src/scripts/promo-slider.js
@@ -0,0 +1,22 @@
+/**
+ * Created by neyia on 10.06.2016.
+ */
+$(document).ready(function(){
+
+    $('.promo-slider__pagination a').click(function(e) {
+        e.preventDefault();
+
+        $('.promo-slider__pagination a').removeClass('link--underline-active');
+        $(this).addClass('link--underline-active');
+
+        var tab = $(this).attr('href');
+        $('.promo-slider__item').not(tab).css({'display':'none'});
+
+        var tab2 = $(this).attr('href')+'0';
+
+        $('.healthy-rest__slide-item').css({'display':'block'}).not(tab2).css({'display':'none'});
+
+        $(tab).fadeIn(400);
+    });
+
+});
